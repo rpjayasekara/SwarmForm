0a2a5e1af34c8bcd0695eb094675cb457d2f55e7
rpjayasekara <rpjayasekara.16@cse.mrt.ac.lk>
2020-04-28 09:48:00 +0530

    Added DAG class and Node class

--- swarmform/examples/swarmpad_examples/swarmpad.py
+++ swarmform/examples/swarmpad_examples/swarmpad.py
@@ -1,24 +1,39 @@
-from swarmform import SwarmPad
-from fireworks import Firework, ScriptTask, FWorker
-from fireworks.core.rocket_launcher import launch_rocket
+# from swarmform import SwarmPad
+# from fireworks import Firework, ScriptTask, FWorker
+# from fireworks.core.rocket_launcher import launch_rocket
+#
+# if __name__ == "__main__":
+# 	# set up the SwarmPad and reset it
+# 	swarmpad = SwarmPad()
+# 	swarmpad.reset('', require_password=False)
+# 	#
+# 	# # create the Firework consisting of a custom "Addition" task
+# 	# firework = Firework(ScriptTask.from_str('echo "hello"'))
+# 	#
+# 	# # store workflow
+# 	# swarmpad.add_sf(firework)
+# 	#
+# 	# # Retrieve SwarmFlow from the SwarmPad and print
+# 	# sf = swarmpad.get_sf_by_id(1)
+# 	# print(sf.to_db_dict())
+# 	#
+# 	# sf = swarmpad.get_sf_by_name('Unnamed FW')
+# 	# print(sf.to_db_dict())
+# 	#
+# 	# # Run the swarmFlow
+# 	# launch_rocket(swarmpad, FWorker())
 
-if __name__ == "__main__":
-	# set up the SwarmPad and reset it
+from swarmform import SwarmPad, WorkflowGenerator
+
+
+def main():
+	# set up the LaunchPad and reset it
 	swarmpad = SwarmPad()
 	swarmpad.reset('', require_password=False)
+	workflow_dax = "/Users/randika/Documents/WorkflowSim-1.0/config/dax/Montage_100.xml"
+	dax_swarmflow = WorkflowGenerator.generate_workflow(workflow_dax)
+	swarmpad.add_sf(dax_swarmflow)
 
-	# create the Firework consisting of a custom "Addition" task
-	firework = Firework(ScriptTask.from_str('echo "hello"'))
 
-	# store workflow
-	swarmpad.add_sf(firework)
-
-	# Retrieve SwarmFlow from the SwarmPad and print
-	sf = swarmpad.get_sf_by_id(1)
-	print(sf.to_db_dict())
-
-	sf = swarmpad.get_sf_by_name('Unnamed FW')
-	print(sf.to_db_dict())
-
-	# Run the swarmFlow
-	launch_rocket(swarmpad, FWorker())
+if __name__ == "__main__":
+	main()
